77de59b38bfbe7ee522f0243ca86c1b911bdb3ad
Krishna <coderks36@gmail.com>
2025-04-27 19:09:07 +0530

    app.js server setup

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,22 @@
 import express from "express";
-import {createServer} from "node:http";
-
-import {Server} from "socker.io";
-
-import mongoose from " mongoose";
-
+import { createServer } from "node:http";
+import { Server } from "socket.io";   
+import mongoose from "mongoose";      
 import cors from "cors";
 
 const app = express();
-const sever=createServer(app);
+const server = createServer(app);    
 
-app.get("/home",(req,res)=>{
-    return res.json({"hello":"world"});
-})
+app.use(cors());                      // optional, but usually needed when using APIs
+
+app.get("/home", (req, res) => {
+  return res.json({ "hello": "world" });
+});
+
+const start = async () => {
+  app.listen(8000, () => {
+    console.log("Listening to port 8000");
+  });
+};
+
+start();
