2b72f4b96605688e5cdf230af83941ba8a8ec8c4
Krishna <coderks36@gmail.com>
2025-04-30 21:46:34 +0530

    app.js env configured

--- backend/app.js
+++ backend/app.js
@@ -3,30 +3,38 @@ import { createServer } from "node:http";
 import { Server } from "socket.io";   
 import mongoose from "mongoose";      
 import cors from "cors";
-import {connectToSocket} from "./src/controllers/socketmanager"
+import dotenv from "dotenv";
+dotenv.config();
+// import {connectToSocket} from "./src/controllers/socketmanager"
+import userRoutes from "./src/routes/userroute.js"
 
 const app = express();
 const server = createServer(app);
 const io = new Server(server, );    
 
-app.use(cors());                      // optional, but usually needed when using APIs
 
+app.use(cors());                      // optional, but usually needed when using APIs
 app.set("port",(process.env.PORT || 8000));
+app.use(express.json({limit:"40kb"}));
+app.use(express.urlencoded({limit:"40kb",extended:true}));
+
+
+app.use("/api/v1/users",userRoutes);
+
+
 
 
 // app.get("/home", (req, res) => {
 //   return res.json({ "hello": "world" });
 // });
 
-app.use(express.json({limit:"40kb"}));
-app.use(express.urlencoded({limit:"40kb",extended:true}));
 
 
 const start = async () => {
   app.set("mongo_user")
-  const connectionDb = await mongoose.connect("mongodb+srv://krishnarachutkar15:fRHpaQdtxsGHY9OA@clustor0.4rnk05o.mongodb.net/")
+  const connectionDb = await mongoose.connect(process.env.MONGO_URL);
 
-  console.log(`Mongo connected DB Host: ${connectionDb.connection.host}`)
+  console.log(`Mongo connected DB Host: ${connectionDb.connection.host}`);
 
 
   server.listen(app.get("port"), () => {
